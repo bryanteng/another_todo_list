22fcaebcab2cd3ec0089707d08b49d5d228c7f55
Bryan Teng <bteng1226@gmail.com>
2019-03-13 11:57:55 -0500

    able to add and edit a todo

--- src/components/list.js
+++ src/components/list.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
 
 class list extends Component {
+
+    state = {
+        title: "My Todos",
+        todos: []
+    }
+
+    handleButtonClick = ()=>{
+        this.setState({ todos: [...this.state.todos, "a todo"]})
+    }
+
+    handleTodoClick = (event) =>{
+        console.log(event.target.id)
+    }
+
+    handleOnChange = (event) =>{
+        console.log(event.target.id)
+        let changed = this.state.todos.map( (todo,index) => {
+            if(index == event.target.id){
+                return todo = event.target.value
+            }else{
+                return todo
+            }
+        })
+        this.setState({ todos: changed})
+    }
+
     render() {
         return (
             <div>
-                a list
+                <div class="outer">
+                    <div class="inner">{this.state.title}</div>
+                    <button class="inner" onClick={this.handleButtonClick}> add a todo </button>
+                </div>
+                <ul>
+                    {this.state.todos ? this.state.todos.map((todo,index) => <li onClick={this.handleTodoClick}><input id={index} onChange={this.handleOnChange} value={todo} /></li>) 
+                    : null }
+                </ul>
             </div>
         );
     }
