06ed88f0899a6208f821f29e73dee0c5305f800c
Bryan Teng <bteng1226@gmail.com>
2019-03-13 11:20:44 -0500

    removed unneeded files and started basic setup

---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class list extends Component {
+    render() {
+        return (
+            <div>
+                a list
+            </div>
+        );
+    }
+}
+
+export default list;
---
+++ src/containers/lists.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import List from '../components/list'
+
+class lists extends Component {
+    render() {
+        return (
+            <div>
+                my lists
+                <List />
+            </div>
+        );
+    }
+}
+
+export default lists;
