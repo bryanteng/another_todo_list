9b7545cc089ec2ed24ead1171e137a0cefa8ed92
Bryan Teng <bteng1226@gmail.com>
2019-03-14 14:57:11 -0500

    added draggable properties and functions, fixing drag values

--- src/components/list.js
+++ src/components/list.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import '../styles/list.css'
 
 class list extends Component {
@@ -14,7 +14,7 @@ class list extends Component {
 
     handleOnChange = (event) =>{
         let changed = this.state.todos.map( (todo,index) => {
-            if(index == event.target.id){
+            if(index === ~~event.target.id){
                 return todo = event.target.value
             }else{
                 return todo
@@ -23,18 +23,22 @@ class list extends Component {
         this.setState({ todos: changed})
     }
 
+    handleTitleClick = (event) =>{
+        this.setState({title: event.target.value})
+    }
+
     render() {
         return (
-            <div class="list">
+            <Fragment class="list">
                 <div class="outer">
-                    <div class="inner">{this.state.title}</div>
+                    <div class="inner"><input class="input" onChange={this.handleTitleClick} width={`${this.state.title.length}`} value={this.state.title}/></div>
                     <button class="inner" onClick={this.handleButtonClick}> todo++ </button>
                 </div>
                 <ul class="outer">
                     {this.state.todos ? this.state.todos.map((todo,index) => <li><input class="input" id={index} onChange={this.handleOnChange} value={todo} /></li>) 
                     : null }
                 </ul>
-            </div>
+            </Fragment>
         );
     }
 }
--- src/containers/lists.js
+++ src/containers/lists.js
@@ -1,23 +1,54 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import List from '../components/list'
 import '../styles/list.css'
 
 class lists extends Component {
     state ={
-        lists:[]
+        lists:[],
+        draggedItem: {},
+        counter: 0
     }
 
-    handleButtonClick = () =>{
-        this.setState({lists: [... this.state.lists, <List />]})
-        console.log(this.state.lists)
+    handleButtonClick = (event) =>{
+        this.setState({lists: [...this.state.lists,{list: <List/>, id: this.state.counter}], counter: this.state.counter+1 })
+        console.log(event.target.className)
+    }
+
+    handleDrag = (event)=>{
+        console.log(event.dataTransfer.items, "drag")
+        this.setState({draggedItem: event.target})
+    }
+
+    handleDrop = (event) =>{
+        event.preventDefault()
+        console.log(event.target, "this is drop")
+        let filtered = this.state.lists.filter(list => list.id !== ~~event.target.id)
+        this.setState({draggedItem: event.target, lists: filtered})
+    }
+
+    handleDragover = (event) =>{
+        event.preventDefault()
+        console.log(this.state.draggedItem, "this is dragged item when dragover")
+        if(event.target.id && event.target.className === "list"){
+            if(event.target.id < this.state.draggedItem.id){
+                console.log("item is moving left")
+                let lists=this.state.lists
+                let temp = [...lists].slice(0,event.target.id)
+                temp.push(this.state.draggedItem)
+                temp = temp.concat(lists.slice(event.target.id, lists.length-1))
+                this.setState({lists: temp})    
+            }else{
+                console.log("item is moving right")
+            }
+        }
     }
 
     render() {
         return (
             <div class="myLists">
-                my lists <button onClick={this.handleButtonClick}>list++</button>
+                my lists <button class="imaButton" onClick={this.handleButtonClick}>list++</button>
                 <div class="lists">
-                    {this.state.lists ? this.state.lists.map(list => <Fragment >{list}</Fragment>) : null }
+                    {this.state.lists ? this.state.lists.map((list,index) => <div class="list" draggable="true" onDragStart={this.handleDrag} onDragOver={this.handleDragover} onDrop={this.handleDrop} id={index} >{list.list}</div>) : null }
                 </div>
             </div>
         );
